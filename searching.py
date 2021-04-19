7d42f5ff5612355a58636a97a73cab8588f9d9df
TanaKohu <tanakohu@gmail.com>
2021-04-19 10:58:07 +0200

    created dna reader

--- searching.py
+++ searching.py
@@ -26,8 +26,42 @@ def read_data(file_name, key):
     return sequences[key]
 
 
+def linear_search(file_name, number):
+    positions = []
+    count = 0
+    i=0
+
+    while i < len(file_name):
+        if file_name[i] == number:
+            positions.append(i)
+            count = count + 1
+        i = i + 1
+
+    return {"positions" : positions, "count" : count}
+
+
+def pattern_search(sequence, pattern):
+    position = set()
+    p_size = len(pattern)
+    start = 0
+    end = p_size
+
+    while end < len(sequence):
+        if pattern == sequence[start:end]:
+            position.add(start + p_size // 2)
+
+        start = start + 1
+        end = end + 1
+
+    return position
+
+
 def main():
-    read_data(file_name="sequential.json", key="unordered_numbers")
+    file1 = read_data(file_name="sequential.json", key="unordered_numbers")
+    file2 = read_data(file_name="sequential.json", key="dna_sequence")
+    print(file1)
+    print(linear_search(file1, number=9))
+    print(pattern_search(file2, pattern="ATA"))
 
 
 if __name__ == '__main__':
